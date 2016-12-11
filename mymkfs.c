f69f93ecdb5c4fc619c71a23265232d3331d591c
LeeJiSeon <j5119s@naver.com>
2016-12-12 00:05:11 +0900

    Add files via upload

--- mymkfs.c
+++ mymkfs.c
@@ -1,59 +1,25 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <time.h>
-typedef struct bt_block{
-	char boot[2];
-}bt_block;
+int main()
+{
 
-typedef struct super_block{
-	char si[64];
-	char sd[128];
-}s_block;
-
-typedef struct inode_list{
-	int inode_num : 10;
-	int kind : 1;
-	int cp : 22;
-	int inode_db : 10;
-	int inode_sib : 10;
 	struct tm *t;
-}inode_list;
-
-typedef struct data_block{
-	int data_num;
-	char data[124];
-}data_block;
-
-typedef struct file_system{
-	bt_block bootblock;
-	s_block superblock;
-	inode_list inodelist;
-	data_block datablock;
-}file_system;
-
-int main(){
 	time_t now;
 	now=time(NULL);
-	file_system fs;
-	FILE * ofp=fopen("myfs", "r");
-	if(ofp!=NULL)
+	t=localtime(&now);
+ 
+	FILE *inp, *oup; //인풋, 아웃풋 파일
+	int check;
+	if((inp = fopen("myfs", "r"))==NULL)
+		inp = fopen("myfs", "w");
+	else
 	{
-		printf("myfs is already existed");
+		printf("myfs exists.\n");
+		//fread();
 		return -1;
 	}
-	fclose(ofp);
-
-	ofp=fopen("myfs", "w");
-	fs.superblock.si[0]+=128;
-	fs.superblock.sd[0]+=128;
-	fs.inodelist.inode_num=1;
-	fs.inodelist.kind=1;
-	fs.inodelist.cp=0;
-	fs.inodelist.inode_db=1;
-	fs.inodelist.t=localtime(&now);
-	fwrite(&fs,sizeof(file_system),1,ofp);
-	fclose(ofp);
+	//fwrite();
 	return 0;
+
 }
-
-	
-
